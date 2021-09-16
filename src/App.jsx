4ae123bf48622be483ba7679d453b936f692dc6e
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-16 17:25:05 +0300

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { Switch, Route } from 'react-router-dom';
+import css from './App.module.css';
+import Navigation from './components/Navigation';
+import HomePage from './components/HomePage';
+import MoviesPage from './components/MoviesPage';
+import MovieDetailsPage from './components/MovieDetailsPage';
+import PageError from './components/PageError';
+
 function App() {
-  return <div className="App">MyProject</div>;
+  return (
+    <div className={css.container}>
+      <Navigation />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/movies" exact>
+          <MoviesPage />
+        </Route>
+        <Route path="/movies/:movieId">
+          <MovieDetailsPage />
+        </Route>
+        <Route>
+          <PageError />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
