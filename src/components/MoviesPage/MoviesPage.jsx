5d299a949808b1c64df5dc45f15404a4c5d28d99
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-17 20:28:08 +0300

    added component

--- src/components/MoviesPage/MoviesPage.jsx
+++ src/components/MoviesPage/MoviesPage.jsx
@@ -1,10 +1,28 @@
-import PropTypes from 'prop-types';
+import { useState } from 'react';
 import css from './MoviesPage.module.css';
 
 function MoviesPage(props) {
-  return <div></div>;
+  const [query, setQuery] = useState('');
+
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   const currentQuery = query.trim();
+  //   if (!currentQuery) return;
+
+  //  };
+
+  return (
+    <div className={css.container}>
+      <form>
+        <input
+          type="text"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
 
 export default MoviesPage;
-
-MoviesPage.propTypes = {};
---
+++ src/components/PageError/PageError.jsx
@@ -0,0 +1,10 @@
+// import PropTypes from 'prop-types';
+// import css from './PageError.module.css';
+
+function PageError(props) {
+  return <div></div>;
+}
+
+export default PageError;
+
+PageError.propTypes = {};
