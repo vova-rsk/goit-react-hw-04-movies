7d261fefaba193d9c135fe7f28ad6a1d2f13c59d
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-17 20:25:30 +0300

    added new functions

--- src/services/themoviedb-api.js
+++ src/services/themoviedb-api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API_KEY = 'd81df9c293a8c58366ed983adfddab04';
 const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const MEDIA_TYPE = 'movie';
 const TIME_WINDOW = 'day';
 
@@ -19,9 +20,26 @@ const fetchMoviesDetails = async id => {
   return response;
 };
 
-const fetchMoviesCredits = () => {};
+const fetchMoviesCredits = async id => {
+  const url = `${BASE_URL}/${MEDIA_TYPE}/${id}/credits?api_key=${API_KEY}&language=en-US`;
+  const response = await axios.get(url);
+  return response;
+};
 
-const fetchMoviesReviews = () => {};
+const fetchMoviesReviews = async id => {
+  const url = `${BASE_URL}/${MEDIA_TYPE}/${id}/reviews?api_key=${API_KEY}&language=en-US&page=1`;
+  const response = await axios.get(url);
+  return response;
+};
+
+const profilePathMaker = data => {
+  if (!data.length) return data;
+  return data.map(actor => {
+    const { profile_path: path } = actor;
+    const updatedPath = path ? `${BASE_IMG_URL}${path}` : path;
+    return { ...actor, profile_path: updatedPath };
+  });
+};
 
 const api = {
   fetchGetTrending,
@@ -29,6 +47,7 @@ const api = {
   fetchMoviesDetails,
   fetchMoviesCredits,
   fetchMoviesReviews,
+  profilePathMaker,
 };
 
 export default api;
