afe3bfe3901d260786cc0f4bc8385eb11b7ebf23
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-17 20:22:23 +0300

    added component

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,10 +1,62 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import css from './Cast.module.css';
+import themoviedbApi from '../../services/themoviedb-api';
+import defaultProfile from './default-profile.jpeg';
 
-function Cast(props) {
-  return <div></div>;
+const ACTORS_SHOW_LIMIT = 10;
+
+function Cast({ movieId }) {
+  const [cast, setCast] = useState([]);
+
+  useEffect(() => {
+    themoviedbApi
+      .fetchMoviesCredits(movieId)
+      .then(response => {
+        const actors = themoviedbApi.profilePathMaker(response.data.cast);
+        setCast(actorsFiltering(actors));
+      })
+      .catch(error => console.log(error.message));
+  }, [movieId]);
+
+  /*func for gettinq first N actors from the total number*/
+  const actorsFiltering = actors => {
+    return actors.reduce((acc, actor) => {
+      const { known_for_department: role } = actor;
+      if (acc.length >= ACTORS_SHOW_LIMIT || role !== 'Acting') return [...acc];
+      return [...acc, actor];
+    }, []);
+  };
+
+  return (
+    cast.length && (
+      <div>
+        <ul>
+          {cast.map(({ id, name, character, profile_path: profilePath }) => (
+            <li key={id}>
+              <div>
+                <p>{name}</p>
+                <p>Character: {character}</p>
+              </div>
+              <div className={css.imgThumb}>
+                <img
+                  src={profilePath ?? defaultProfile}
+                  alt={name}
+                  className={css.image}
+                  width="120"
+                  height="160"
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  );
 }
 
 export default Cast;
 
-Cast.propTypes = {};
+Cast.propTypes = {
+  movieId: PropTypes.string,
+};
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,10 +1,45 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import css from './Reviews.module.css';
+// import css from './Reviews.module.css';
+import themoviedbApi from '../../services/themoviedb-api';
 
-function Reviews(props) {
-  return <div></div>;
+function Reviews({ movieId }) {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    themoviedbApi
+      .fetchMoviesReviews(movieId)
+      .then(response => {
+        console.log(response);
+        setReviews(response.data.results);
+      })
+      .catch(error => console.log(error.message));
+  }, [movieId]);
+
+  if (reviews.length) {
+    return (
+      <div>
+        <ul>
+          {reviews.map(({ id, author, content }) => (
+            <li key={id}>
+              <h5>Author: {author}</h5>
+              <p>{content}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>We dont have any reviews for this movie</p>
+      </div>
+    );
+  }
 }
 
 export default Reviews;
 
-Reviews.propTypes = {};
+Reviews.propTypes = {
+  movieId: PropTypes.string,
+};
