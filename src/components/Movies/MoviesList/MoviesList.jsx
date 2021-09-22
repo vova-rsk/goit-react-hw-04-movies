3e4f2966abbc5ff6ec52cb57931f7d727611f132
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-22 17:09:27 +0300

    refactoring

--- src/components/Movies/MoviesList/MoviesList.jsx
+++ src/components/Movies/MoviesList/MoviesList.jsx
@@ -1,15 +1,20 @@
-import { Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import css from './MoviesList.module.css';
 
-const MoviesList = ({ url, hash, movies }) => {
+const MoviesList = ({ url, movies }) => {
+  const location = useLocation();
+
   return (
     <div className={css.container}>
       <ul>
         {movies.map(movie => (
           <li key={movie.id} className={css.item}>
             <Link
-              to={{ pathname: `${url}/${movie.id}`, state: { from: hash } }}
+              to={{
+                pathname: `${url}/${movie.id}`,
+                state: { from: { ...location } },
+              }}
               className={css.link}
             >
               {movie.title}
@@ -26,5 +31,4 @@ export default MoviesList;
 MoviesList.propTypes = {
   url: PropTypes.string.isRequired,
   movies: PropTypes.array.isRequired,
-  hash: PropTypes.object.isRequired,
 };
